0c0e1bc70480bc748701b6bf540d2bbca2db4bdb
Goose <gdripoll@gmail.com>
2020-02-12 13:52:01 -0300

    ui-1

--- src/components/trayin/index.js
+++ src/components/trayin/index.js
@@ -1,25 +1,52 @@
 import React, { useState } from 'react';
-import { Button } from '@material-ui/core';
+
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import { Button, Grid, Paper, Box } from '@material-ui/core';
+
+
+const styles = makeStyles(theme => ({
+    root: {
+    },
+    textField: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        marginTop: theme.spacing(10),
+    }
+}));
+
 
 const Trayin = (props) => {
 
     const [input, setInput] = useState(props.text);
-    let textArea= React.createRef();
+    let textArea = React.createRef();
 
     const handleInputChange = e => {
         setInput(e.target.value)
     }
 
-    const handleClick = () =>{
+    const handleClick = () => {
         props.handleText(input)
     }
 
-    
     return (
-        <div>
-            <p><textarea ref={textArea} defaultValue={ input } onChange={handleInputChange} style={ {height:'300px', width:'600px'}}></textarea></p>
+        <Box m={1}>
+            {/* <Paper variant="outlined" p={12}> */}
+            <TextField
+                id="outlined-multiline-static"
+                label="Párrafo"
+                multiline
+                fullWidth
+                rows="6"
+                defaultValue={input}
+                variant="outlined"
+                onChange={handleInputChange}
+                className={styles.textField}
+            />
+            <Box m={1}>
             <Button variant="contained" onClick={handleClick}>Analizar</Button>
-        </div>
+            {/* </Paper> */}
+            </Box>
+        </Box>
     );
 }
 
--- src/container/analisis/index.js
+++ src/container/analisis/index.js
@@ -3,6 +3,15 @@ import React, { useState } from 'react';
 import Trayin from '../../components/trayin';
 import Counter from '../../components/counter';
 
+import { makeStyles } from '@material-ui/core/styles';
+import { Container } from '@material-ui/core';
+
+const styles = makeStyles(theme => ({
+    root: {
+        margin: 30
+    },
+}));
+
 export default function Analisis() {
 
     const [text, setText] = useState('inicial papa pepe popo pipi papa pa pepito');
@@ -12,10 +21,10 @@ export default function Analisis() {
     }
 
     return (
-        <div style={ {border: 'solid 1px red'} }>
-            <Trayin text={text} handleText={handleTextChange}/>
-            <Counter text={text}/>
-        </div>
+        <Container className={styles.root}>
+            <Trayin text={text} handleText={handleTextChange} />
+            <Counter text={text} />
+        </Container>
     );
 
 }
